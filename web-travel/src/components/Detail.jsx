f448993cedb28fc53271f6c8be65f4398dee51b2
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-07-30 11:28:34 +0700

    feat(detail): add gallery section on detail packages

--- web-travel/src/components/Detail.jsx
+++ web-travel/src/components/Detail.jsx
@@ -5,74 +5,85 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Detail = () => {
-	const { id } = useParams();
-	const [trip, setTrip] = useState(null);
+  const { id } = useParams();
+  const [trip, setTrip] = useState(null);
 
-	useEffect(() => {
-		const fetchDetailTrip = async () => {
-			const tripData = await getDetailTrip(id);
-			setTrip(tripData);
-		};
+  useEffect(() => {
+    const fetchDetailTrip = async () => {
+      const tripData = await getDetailTrip(id);
+      setTrip(tripData);
+    };
 
-		fetchDetailTrip();
-	}, [id]);
+    fetchDetailTrip();
+  }, [id]);
 
-	if (!trip) {
-		return <div>Loading...</div>;
-	}
+  if (!trip) {
+    return <div>Loading...</div>;
+  }
 
-	return (
-		<>
-			<section
-				id="detailPackages"
-				className="px-7 sm:px-10 py-24 flex flex-col items-center bg-gradient-to-b from-neutral-800 via-slate-50 to-slate-50"
-			>
-				<header className="text-left mb-8 w-full md:max-w-2xl lg:max-w-5xl flex items-center gap-4 justify-between">
-					<h1 className="text-3xl font-bold md:text-4xl lg:text-5xl text-slate-100">
-						Detail Package
-					</h1>
-				</header>
-				<div
-					key={trip.id}
-					className="flex flex-col md:flex-row gap-7 p-4 rounded-lg shadow w-full max-w-5xl text-neutral-800 bg-slate-50"
-				>
-					<img
-						src={trip.img}
-						alt={trip.name}
-						className="object-cover object-center h-full max-h-[30rem] w-full md:w-1/2 rounded-md mb-2"
-					/>
-					<div className="detail flex flex-col w-full flex-1 self-center">
-						{spanLabel(trip.typeTrip)}
-						<h2 className="text-4xl font-semibold">{trip.name}</h2>
-						<p className="text-lg">{trip.location}</p>
-						<h3 className="mt-4">Benefits:</h3>
-						<ul>
-							{trip.benefits.map((benefit, index) => (
-								<li key={index}>{benefit}</li>
-							))}
-						</ul>
-						<span className="mt-6 flex items-center justify-between">
-							<p className="text-2xl font-bold">IDR {trip.price}K</p>
-							<Link
-								to="#"
-								className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-neutral-800 rounded-lg bg-slate-50 border-2 border-neutral-800 hover:bg-slate-300 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 w-full max-w-40 text-nowrap"
-							>
-								{/* <i className="mr-3 fa-solid fa-arrow-left"></i> */}
-								Order Now
-							</Link>
-						</span>
-					</div>
-				</div>
-				<Link
-					to="/#packages"
-					className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-neutral-800 rounded-lg bg-slate-50 border-2 border-neutral-800 hover:bg-slate-300 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 w-full max-w-40 mt-8 text-nowrap"
-				>
-					<i className="mr-3 fa-solid fa-arrow-left"></i>
-					Back to Home
-				</Link>
-			</section>
-		</>
-	);
+  return (
+    <>
+      <section
+        id="detailPackages"
+        className="px-7 sm:px-10 py-24 flex flex-col items-center bg-gradient-to-b from-neutral-800 via-slate-50 to-slate-50"
+      >
+        <header className="text-left mb-8 w-full max-w-5xl flex items-center gap-4 justify-between">
+          <h1 className="text-3xl font-bold md:text-4xl lg:text-5xl text-slate-100">
+            Detail Package
+          </h1>
+        </header>
+        <div
+          key={trip.id}
+          className="flex flex-col md:flex-row gap-7 p-4 rounded-lg shadow w-full max-w-5xl text-neutral-800 bg-slate-50 "
+        >
+          <img
+            src={trip.img}
+            alt={trip.name}
+            className="object-cover object-center h-full max-h-[34rem] w-full md:w-1/2 rounded-md mb-2"
+          />
+          <div className="py-5 detail flex flex-col w-full flex-1 self-center overflow-hidden	">
+            {spanLabel(trip.typeTrip)}
+            <h2 className="text-4xl font-semibold">{trip.name}</h2>
+            <p className="text-lg">{trip.location}</p>
+            <h3 className="mt-4">Benefits:</h3>
+            <ul>
+              {trip.benefits.map((benefit, index) => (
+                <li key={index}>{benefit}</li>
+              ))}
+            </ul>
+            <span className="mt-4 flex items-center justify-between">
+              <p className="text-2xl font-bold">IDR {trip.price}K</p>
+              <Link
+                to="#"
+                className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-neutral-800 rounded-lg bg-slate-50 border-2 border-neutral-800 hover:bg-slate-300 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 w-full max-w-40 text-nowrap"
+              >
+                {/* <i className="mr-3 fa-solid fa-arrow-left"></i> */}
+                Order Now
+              </Link>
+            </span>
+            <div className="my-5 w-full border border-gray-300 h-0"></div>
+            <h3 className="mb-2 text-xl font-semibold">Gallery</h3>
+            <span className="overflow-auto flex items-center gap-5">
+              {trip.gallery.map((image, index) => (
+                <img
+                  src={image}
+                  key={index}
+                  className="min-w-52 h-32 rounded-xl object-cover object-center"
+                ></img>
+              ))}
+            </span>
+          </div>
+        </div>
+        <Link
+          to="/#packages"
+          className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-neutral-800 rounded-lg bg-slate-50 border-2 border-neutral-800 hover:bg-slate-300 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 w-full max-w-40 mt-8 text-nowrap"
+        >
+          <i className="mr-3 fa-solid fa-arrow-left"></i>
+          Back to Home
+        </Link>
+      </section>
+    </>
+  );
 };
 
 export default Detail;
--- web-travel/src/components/ServicesSection.jsx
+++ web-travel/src/components/ServicesSection.jsx
@@ -1,39 +1,50 @@
 const ServicesSection = () => {
   return (
-    <section id="services" className="px7 sm:px-10 my-24 grid place-content-center">
+    <section
+      id="services"
+      className="px7 sm:px-10 my-24 grid place-content-center"
+    >
       <header className="text-left mb-8">
-        <h1 className="text-3xl font-bold md:text-4xl lg:text-5xl">Our Services</h1>
+        <h1 className="text-3xl font-bold md:text-4xl lg:text-5xl">
+          Our Services
+        </h1>
         <p className="text-base md:text-lg font-semibold text-gray-600">
           Top Benefits when using our services
         </p>
       </header>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 place-content-center place-items-center w-full max-w-max">
-        <div className="flex flex-col items-center text-center p-4 rounded-lg shadow w-full max-w-64">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 place-content-center w-full max-w-max">
+        <div className="flex flex-col justify-center items-center text-center p-4 rounded-lg shadow w-full max-w-64">
           <div className="text-4xl mb-4">👤</div>
           <h2 className="text-xl font-semibold mb-2">Best Tour Guide</h2>
           <p>
-          The best tour guides have an in-depth understanding of the mountain terrain, trekking trails, geography, and regulations that apply in national parks.
+            The best tour guides have an in-depth understanding of the mountain
+            terrain, trekking trails, geography, and regulations that apply in
+            national parks.
           </p>
         </div>
-        <div className="flex flex-col items-center text-center p-4 rounded-lg shadow w-full max-w-64">
+        <div className="flex flex-col items-center justify-center text-center p-4 rounded-lg shadow w-full max-w-64">
           <div className="text-4xl mb-4">📋</div>
           <h2 className="text-xl font-semibold mb-2">Lots of Choices</h2>
           <p>
-          We offer a wide selection of destinations and travel packages to suit your preferences and needs.
+            We offer a wide selection of destinations and travel packages to
+            suit your preferences and needs.
           </p>
         </div>
-        <div className="flex flex-col items-center text-center p-4 rounded-lg shadow w-full max-w-64">
+        <div className="flex flex-col items-center justify-center text-center p-4 rounded-lg shadow w-full max-w-64">
           <div className="text-4xl mb-4">❤️</div>
           <h2 className="text-xl font-semibold mb-2">Secure & Trusted</h2>
           <p>
-          We put safety and trust in every detail of your trip. With proven and trusted services, you can explore the world with peace of mind.
+            We put safety and trust in every detail of your trip. With proven
+            and trusted services, you can explore the world with peace of mind.
           </p>
         </div>
-        <div className="flex flex-col items-center text-center p-4 rounded-lg shadow w-full max-w-64">
+        <div className="flex flex-col justify-center items-center text-center p-4 rounded-lg shadow w-full max-w-64">
           <div className="text-4xl mb-4">🗂</div>
           <h2 className="text-xl font-semibold mb-2">Easy Booking</h2>
           <p>
-          Our easy and fast booking process ensures you can plan your vacation without a hitch. Discover an unforgettable adventure with our professional and reliable services!
+            Our easy and fast booking process ensures you can plan your vacation
+            without a hitch. Discover an unforgettable adventure with our
+            professional and reliable services!
           </p>
         </div>
       </div>
