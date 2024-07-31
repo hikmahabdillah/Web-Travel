aacba3e8064ff70dfd35f060363a0c97706c72cf
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-07-31 14:18:30 +0700

    feat(server): connect to mongodb

--- web-travel/server/server.js
+++ web-travel/server/server.js
@@ -2,9 +2,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./routes/users.js";
+import mongoose from "mongoose";
 
 dotenv.config();
-
 const usersRoutes = router;
 
 // express app
@@ -12,21 +12,28 @@ const app = express();
 
 // middleware
 app.use(express.json());
-
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
-const port = process.env.PORT || 4001;
-//  listen for request
-app.listen(port, () => {
-  console.log("listening on port", port);
-});
-
 // routes
-app.use('/api/users', usersRoutes);
-
+app.use("/api/users", usersRoutes);
 app.get("/", (req, res) => {
   res.json({ msg: "Welcome to the app" });
 });
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    const port = process.env.PORT || 4001;
+    // listen for request
+    app.listen(port, () => {
+      console.log(`Connected to DB & listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // exit the process if connection fails
+  });
